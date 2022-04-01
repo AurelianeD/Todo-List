d55fb1c666de7790c327dc7e0b2bbacc7a1423cf
aureliane dor <aureliane.dor@gmail.com>
2022-04-01 15:50:18 +0200

    add log-in and sign-in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :authorized, only: [:new, :create, :welcome]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to '/index'
+    else
+       redirect_to '/login'
+    end
+  end
+
+  def welcome
+  end
+
+  def login
+  end
+
+  def page_required_login
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+  get 'index', to: 'todo_lists#index'
+
   resources :todo_lists do
     resources :todo_items do
       member do
@@ -9,5 +16,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "todo_lists#index"
+  root "sessions#welcome"
 end
